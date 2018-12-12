e05b7d768982c1c94781e5758315b505ff8ad5f7
anupdhml <anupdhml+git@gmail.com>
2018-12-12 00:18:30 -0500

    Final refactor of PS1 script

--- data/bin/.personal_bash_prompt.sh
+++ data/bin/.personal_bash_prompt.sh
@@ -1,101 +1,86 @@
 #!/usr/bin/env bash
 # Generate a useful PS1 for personal use
 
-# Functions ###################################################################
+# if not a color terminal, set a simple PS1 and exit right away
+if [[ "$TERM" != *"color" ]]; then
+  PS1="\u@\h:\w\$ "
+  return
+fi
 
-# for showing the return status of last-ran command
-__smileyfunct() {
-  local ret_val=$?
-  if [ "$ret_val" = "0" ]; then
-    echo "|^◡^|"
-  else
-    # print with red
-    echo -e "\e[0;31m|T_T|${ret_val}|\e[0m"
+__personal_prompt() {
+  symbol="$"
+  name="${LOGNAME}@${HOSTNAME%%.*}"
+
+  # colors (dynamic)
+  color_name='\[\033[01;35m\]' #pink
+  color_path='\[\033[01;34m\]' # blue
+  color_git='\[\033[01;30m\]' # black
+
+  # colors (static)
+  COLOR_DANGER='\[\033[01;31m\]' # red
+  COLOR_FAILURE='\[\033[0;31m\]' # red (light)
+  COLOR_RESET='\[\033[00m\]'
+
+  # configs for __git_ps1 function (sourced from /etc/bash_completion.d/git-prompt)
+  GIT_PS1_SHOWUPSTREAM="auto"
+  GIT_PS1_SHOWDIRTYSTATE=yes
+  #GIT_PS1_SHOWUNTRACKEDFILES=yes
+  #GIT_PS1_SHOWSTASHSTATE=yes
+  #GIT_PS1_SHOWCOLORHINTS=yes
+
+  # overrides for home user (set short name)
+  if [ $LOGNAME = anup ]; then
+    # overrides for home user
+    name_list=(anup अनुप ανουπ)
+    name=${name_list[ RANDOM % ${#name_list[@]} ]}
   fi
+
+  # overrides for production hosts
+  if [[ "$HOSTNAME" == *".host."* || "$HOSTNAME" == *".sec."* ]]; then
+    color_path="$COLOR_DANGER"
+  fi
+
+  # overrides for root user
+  if [ "$EUID" -eq 0 ]; then
+    symbol="#"
+    color_name="$COLOR_DANGER" # red
+    color_path="$COLOR_DANGER" # red
+  fi
+
+  ##############################################################################
+
+  # for shortening the work path when it gets long
+  __shorten_path() {
+    local pre= name="$1" length=35;
+    [[ "$name" != "${name#$HOME/}" || -z "${name#$HOME}" ]] &&
+      pre+='~' name="${name#$HOME}" length=$[length-1];
+    ((${#name}>$length)) && name="/...${name:$[${#name}-length+4]}";
+    echo "$pre$name"
+  }
+
+  # main logic
+  set_ps1() {
+      # check the exit code of the previous command and change smileys accordingly
+      if [ $? -eq 0 ]; then
+          local smiley="|^◡^|"
+      else
+          local smiley="${COLOR_FAILURE}|T_T|${COLOR_RESET}"
+      fi
+
+      local name="${color_name}${name}${COLOR_RESET}"
+
+      local path="${color_path}$(__shorten_path "$PWD")${COLOR_RESET}"
+
+      # escape git info properly since they are dervied from user input.
+      # prevents security exploits as in https://github.com/njhartwell/pw3nage
+      __git_info="$(__git_ps1)"
+      local git="${color_git}\${__git_info}${COLOR_RESET}"
+
+      PS1="\n${smiley}${name}:${path}${git}${symbol} "
+  }
+
+  PROMPT_COMMAND="set_ps1${PROMPT_COMMAND:+; $PROMPT_COMMAND}"
 }
 
-# for shortening the work path when it gets long 
-__shorten_path() {
-  local pre= name="$1" length=35;
-  [[ "$name" != "${name#$HOME/}" || -z "${name#$HOME}" ]] &&
-    pre+='~' name="${name#$HOME}" length=$[length-1];
-  ((${#name}>$length)) && name="/...${name:$[${#name}-length+4]}";
-  echo "$pre$name"
-}
-
-# Variables ###################################################################
-
-# Defaults
-name="${LOGNAME}@${HOSTNAME%%.*}"
-prompt_end="$"
-
-# colors
-name_color=$'\e[01;35m'    # pink
-path_color=$'\e[01;34m'    # blue
-git_color=$'\e[01;30m'     # black
-prompt_end_color=$'\e[00m' # black (light)
-
-# configs for __git_ps1 function (sourced from /etc/bash_completion.d/git-prompt)
-GIT_PS1_SHOWUPSTREAM="auto"
-GIT_PS1_SHOWDIRTYSTATE=yes
-#GIT_PS1_SHOWUNTRACKEDFILES=yes
-#GIT_PS1_SHOWSTASHSTATE=yes
-#GIT_PS1_SHOWCOLORHINTS=yes
-
-# overrides for production hosts
-if [[ "$HOSTNAME" == *".host."* || "$HOSTNAME" == *".sec."* ]]; then
-  path_color=$'\e[01;31m' # red
-fi
-
-# overrides for root user
-if [ "$EUID" -eq 0 ]; then
-  prompt_end="#"
-  name_color=$'\e[01;31m' # red
-  path_color=$'\e[01;31m' # red
-fi
-
-# overrides for home user
-if [ $LOGNAME = anup ]; then
-  # overrides for home user
-  name_list=(anup अनुप ανουπ)
-  name=${name_list[ RANDOM % ${#name_list[@]} ]}
-fi
-
-# set fancy prompt only for certain terminals
-case "$TERM" in
-  xterm-color|*-256color) fancy_prompt=yes;;
-esac
-
-###############################################################################
-
-if [ "$fancy_prompt" = yes ]; then
-  #PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
-  PS1='$(__smileyfunct)\[${name_color}\]${name}:\[${path_color}\]$(__shorten_path "$PWD")\[${git_color}\]$(__git_ps1 "(%s)")\[${prompt_end_color}\]${prompt_end} '
-else
-  PS1='\u@\h:\w\$ '
-fi
-
-# empty line before prompt
-PS1="\n$PS1"
-
-## cool prompt
-#function bash_prompt {
-  #if [[ $? -eq "0" ]]; then
-     #SMILEY=" ${LG}^◡^${NONE} ";
-   #else
-     #SMILEY=" ${LR}ಠ_ಠ${NONE} ";
-  #fi
-
-  #local UC=$LG
-  #[ $UID -eq "0" ] && UC=$LR
-
-  #TITLEBAR="${UC}\u${LW}:${LB}${PWD}${NONE}"
-
-  #PS1="$SMILEY${TITLEBAR}${LW} > "
-  #PS2="                                                                                                                                                                                                      "
-  #PS2="${PS2:0:$(( $(echo $PWD | wc -c) + $(whoami | wc -c) + 4))}${EMW} > "
-
-  #echo -n -e "\a"
-#}
-#trap '/bin/echo -n -e "\e[0m"' DEBUG
-#PROMPT_COMMAND=bash_prompt
+__personal_prompt
+unset __personal_prompt
