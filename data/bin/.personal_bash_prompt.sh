3d82a85eeff73dc5043e0d756303ac56b4f7216d
anupdhml <anupdhml+git@gmail.com>
2018-12-12 00:34:43 -0500

    Fix indents

--- data/bin/.personal_bash_prompt.sh
+++ data/bin/.personal_bash_prompt.sh
@@ -14,7 +14,7 @@ __personal_prompt() {
   # colors (dynamic)
   color_name='\[\033[01;35m\]' #pink
   color_path='\[\033[01;34m\]' # blue
-  color_git='\[\033[01;30m\]' # black
+  color_git='\[\033[01;30m\]'  # black
 
   # colors (static)
   COLOR_DANGER='\[\033[01;31m\]' # red
@@ -58,25 +58,25 @@ __personal_prompt() {
     echo "$pre$name"
   }
 
-  # main logic
   set_ps1() {
-      # check the exit code of the previous command and change smileys accordingly
-      if [ $? -eq 0 ]; then
-          local smiley="|^◡^|"
-      else
-          local smiley="${COLOR_FAILURE}|T_T|${COLOR_RESET}"
-      fi
+    # check the exit code of the previous command and change smileys accordingly
+    if [ $? -eq 0 ]; then
+      local smiley="|^◡^|"
+    else
+      local smiley="${COLOR_FAILURE}|T_T|${COLOR_RESET}"
+    fi
 
-      local name="${color_name}${name}${COLOR_RESET}"
+    local name="${color_name}${name}${COLOR_RESET}"
 
-      local path="${color_path}$(__shorten_path "$PWD")${COLOR_RESET}"
+    local path="${color_path}$(__shorten_path "$PWD")${COLOR_RESET}"
 
-      # escape git info properly since they are dervied from user input.
-      # prevents security exploits as in https://github.com/njhartwell/pw3nage
-      __git_info="$(__git_ps1)"
-      local git="${color_git}\${__git_info}${COLOR_RESET}"
+    # escape git info properly since they are dervied from user input.
+    # prevents security exploits as in https://github.com/njhartwell/pw3nage
+    __git_info="$(__git_ps1)"
+    local git="${color_git}\${__git_info}${COLOR_RESET}"
 
-      PS1="\n${smiley}${name}:${path}${git}${symbol} "
+    # finally construct the prompt
+    PS1="\n${smiley}${name}:${path}${git}${symbol} "
   }
 
   PROMPT_COMMAND="set_ps1${PROMPT_COMMAND:+; $PROMPT_COMMAND}"
